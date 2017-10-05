d60b23c756aae857b9adc82b2508edc85e241d7a
hamdhalah <hamdhalah09@gmail.com>
2017-10-05 20:02:39 +0100

    modify server files

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,10 @@
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
-import configFile from '../config/config.json';
+import configFile from '../config/config.js';
 
 const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = configFile[env];
 const db = {};
 
