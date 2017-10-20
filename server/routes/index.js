9ebca1141b1ecdbf3add6f01644b3c0aac5bf17f
Awak Nsico <awaknsikak@gmail.com>
2017-10-20 11:59:35 +0100

    react-installation

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,14 +15,23 @@ const routes = (app) => {
 
   app.post('/api/user/signup', confirmUserInput, checkUserValidity, usersController.signup);
   app.post('/api/user/signin', usersController.signin);
+
   app.get('/api/v1/recipes', recipesController.list);
   app.post('/api/v1/recipes', authenticate.Verify, recipesController.create);
   app.delete('/api/v1/recipes/:recipeId', authenticate.Verify, recipesController.destroy);
   app.put('/api/v1/recipes/:recipeId', authenticate.Verify, recipesController.update);
+  
   app.post('/api/v1/recipes/:recipeId/reviews', authenticate.Verify, reviewsController.create);
   app.get('/api/v1/recipes/:recipeId/reviews', authenticate.Verify, reviewsController.list);
   app.get('/api/v1/recipes/reviews', authenticate.Verify, reviewsController.list);
+
   app.get('/api/v1/users/:userId/recipes', authenticate.Verify, favoriterecipeController.list);
+  app.post('/api/v1/users/:userId/recipes', favoriterecipeController.create);
+  
+
+  app.post('/api/v1/users/upvote/:recipeId', authenticate.Verify, votesController.upvote);
+  app.get('api/recipes?sort=upvotes&order=desc', authenticate.Verify,votesController.list)
+  
 };
 
 export default routes;
---
+++ server/test/faker.js
@@ -0,0 +1,33 @@
+import faker from 'faker';
+
+const fakeData = {
+    signup1: {
+        firstName: 'bestie', 
+        email: 'bestie@bestie.com',
+        lastName:'',
+        password: bestyyy
+
+    },
+
+    user2: {
+        firstName: 'foodie', 
+        email: 'foodie@food.com',
+        lastName:'',
+        password: foodlova
+
+    },
+
+    recipe1: {
+        recipeName: 'rice',
+        ingredients: 'water, rice',
+        description: 'boil rice',
+
+    },
+
+    reviews:{
+        recipeName: 'rice',
+        reviews: 'yummy'
+
+    },
+};
+export default fakeData;
