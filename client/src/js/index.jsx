aed5671acf825efb285041ab658e832fb3e69a69
geekhijabi <bimpeadetunji@rocketmail.com>
2017-10-30 19:06:43 +0100

    Add components

--- client/src/js/index.jsx
+++ client/src/js/index.jsx
@@ -1,20 +1,50 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Match, Miss } from 'react-router';
+import { BrowserRouter, Match, Miss } from 'react-router-dom';
 
-import '../styles/_variables.scss';
-import header from './components/partials/header';
-import NotFound from './components/notfound';
+import './../styles/index.scss';
+// import Header from './components/partials/header';
+// import NotFound from './components/notfound';
+import Home from './sample';
+console.log('Hey')
+/**
+ * 
+ * 
+ * @class Header
+ * @extends {React.Component}
+ */
+class Index extends React.Component {
+/**
+   * @description COnstructor Function
+   * @param {any} props
+   * @memberof Home
+   * @return {void} 
+   */
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     active: true
+  //   };
+  // }
+  /**
+   * 
+   * 
+   *@return {dom} DomElement
+   * @memberof Header
+   */
+  render() {
+    return (
+      <BrowserRouter>
+        <div id="main">
+          <Match exactly pattern="/" component={Home} />
+          {/* <Match pattern="/sample" component={Home} /> */}
+          {/* <Miss component={NotFound} /> */}
 
-const root = () => (
-  <BrowserRouter>
-    <div>
-      <Match exactly pattern="/" component={header} />
-      <Miss component={NotFound} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+console.log('ho ho ho!xmas is here')
 
-    </div>
-  </BrowserRouter>
-);
-
-render(<root />, document.getElementById('root'));
-
+export default Index;
