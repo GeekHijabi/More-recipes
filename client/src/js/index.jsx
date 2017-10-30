3dea3ae4c294e1ddfb7e2bc83abee19657c65cc5
geekhijabi <bimpeadetunji@rocketmail.com>
2017-10-30 12:30:53 +0100

    revamp webpack

--- client/src/js/index.jsx
+++ client/src/js/index.jsx
@@ -1,43 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import { Button } from 'reactstrap';
-import './../styles/index.scss';
+import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 
-/**
- * @description Displays Home View
- * @class Home
- * @extends {React.Component}
- * @param {any} props
- */
-class Home extends React.Component {
-  /**
-   * @description COnstructor Function
-   * @param {any} props
-   * @memberof Home
-   * @return {void} 
-   */
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: true
-    };
-  }
+import '../styles/_variables.scss';
+import header from './components/partials/header';
+import NotFound from './components/notfound';
 
-  /**
-   * @return {dom} DomElement
-   */
-  render() {
-    return (
-      <div>
-        <h1>I am Home</h1>
-        <Button color="success">click me</Button>
-      </div>
-    );
-  }
-}
+const root = () => (
+  <BrowserRouter>
+    <div>
+      <Match exactly pattern="/" component={header} />
+      <Miss component={NotFound} />
 
-ReactDOM.render(
-  <Home />,
-  document.getElementById('root')
+    </div>
+  </BrowserRouter>
 );
+
+render(<root />, document.getElementById('root'));
+
--- server/test/test.spec.js
+++ server/test/test.spec.js
@@ -1,14 +1,17 @@
-import faker from 'faker';
-
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+
+
+import fakeData from './faker';
 import app from '../app';
-import models from '../models';
+import db from '../models';
 
-const expect = chai.expect;
-
-const should = require('chai').should();
+const should = chai.Should();
 chai.use(chaiHttp);
+
+let token;
+let id;
+
 
 describe('More Recipes', () => {
   it('should get the home page', (done) => {
@@ -23,149 +26,98 @@ describe('More Recipes', () => {
         done();
       });
   });
-
-  it('should create a new User', (done) => {
-    chai
-      .request(app)
-      .post('/api/v1/users/signup')
-      .send({
-        username: 'testyyy', email: 'test@gmail.com', password: 'testyyyy'
-      })
-      .res
-      .should
-      .have
-      .status(201)
+  it('should get 404 page', (done) => {
+    chai.request(app)
+      .get('/ap')
       .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
-        res
-          .body
-          .username
-          .should
-          .be('testyyyy');
-        res
-          .body
-          .message
-          .should
-          .be('You have successfully signed up');
+        res.should.have.status(404);
+        done();
+      });
+  });
+
+  describe('User', () => {
+    db.user.destroy({
+      cascade: true,
+      truncate: true
+    });
+    it('should not sign in user with no usernane', (done) => {
+      chai.request(app).post('/api/user/signup')
+        .send(fakeData.noUsernameInput)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.have.property('message').equal('No username supplied');
+          done();
+        });
+    });
+  });
+  it('should create a new User', (done) => {
+    chai.request(app).post('/api/user/signup')
+      .send(fakeData.newUser)
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.body.should.have.property('message').equal('You have successfully created an account');
+        res.should.be.a('object');
         done();
       });
   });
   it('should not create User with invalid email', (done) => {
     chai
       .request(app)
-      .post('/api/v1/users/signup')
-      .send({
-        username: 'password',
-        email: 'fraiser',
-        password: 'password'
-      })
-      .res
-      .should
-      .have
-      .status(409)
+      .post('/api/v1/user/signup')
+      .send(fakeData.noEmailInput)
       .end((err, res) => {
-        if (err) {
-          return done(err);
-        }
         res
           .should
           .have
-          .status(res);
+          .status(404);
         done();
       });
   });
 });
 
-it('should not return password', (done) => {
-  chai
-    .request(app)
-    .post('/api/v1/users/signup')
-    .send({
-      username: 'testtyy',
-      email: 'test@gmail.com',
-      password: 'testrcyy'
-    })
-    .res
-    .should
-    .have
-    .status(201)
-    .end((err, res) => {
-      if (err) {
-        return done(err);
-      }
-      res
-        .body
-        .should
-        .be({
-          // something
-        });
-      done();
-    });
-});
-
 it('should check if password is less 8 characters', (done) => {
-  chai
-    .request(app)
+  chai.request(app).post('/api/user/signup')
+    .send(fakeData.lenPasswordShort)
+    .end((err, res) => {
+      res.should.have.status(400);
+      res.body.should.have.property('message').equal('password must be 8 characters or more');
+      done();
+    });
+});
+
+it('should not let user sign up with the same email/username', (done) => {
+  chai.request(app)
     .post('/api/v1/users/signup')
-    .send({
-      username: 'rowland',
-      email: 'rxxxx@co.com',
-      password: 'ace'
-    })
-    .res
-    .should
-    .have
-    .status(409)
+    .send(fakeData.newUsers)
     .end((err, res) => {
-      if (err) {
-        return done(err);
-      }
-      res
-        .should
-        .have
-        .status(res);
+      res.should.have.status(404);
+      res.body.should.be.a('object');
       done();
     });
 });
 
-it('should return 200 for get request', (done) => {
-  chai
-    .request(app)
-    .post('/api/v1/recipes')
-    .end((err, res) => {
-      res
-        .should
-        .have
-        .status(200);
-      done();
-    });
-});
-
-it('should return 200 for delete request', (done) => {
-  chai
-    .request(app)
-    .delete('/api/v1/recipes')
-    .end((err, res) => {
-      res
-        .should
-        .have
-        .status(200);
-      done();
-    });
-});
-
-it('should return 200 for put request', (done) => {
-  chai
-    .request(app)
-    .add('/api/v1/recipes')
-    .end((err, res) => {
-      res
-        .should
-        .have
-        .status(200);
-      done();
-    });
+describe('Recipes', () => {
+  const recipes = {
+    recipeName: 'Rice',
+    ingredients: 'water rice',
+    description: 'boil rice',
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjdXJyZW50VXNlciIadbnmdhyyQiOjIsInVzZXJuYW1lIjoiaWJyYWhpbSIsImZ1bGxuYW1lIjoidG9wZSBqb3kifSwiaWF0IjoxNTA0NTEzMTE2fQ.FzccsjyPbE9ExFKuhZx4ljZUZKGQjtm3CIZY6sqZ5bY'
+  };
+  it('should get all recipe', (done) => {
+    chai.request(app)
+      .get('/api/v1/recipes')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+  it('should let unauthorized user delete a recipe', (done) => {
+    chai.request(app)
+      .delete(`/api/v1/recipes/${id}`)
+      .end((err, res) => {
+        res.should.have.status(403);
+        done();
+      });
+  });
 });
 
