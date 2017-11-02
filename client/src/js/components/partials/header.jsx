4819d67bcd73efac9bf08de61b1836aeed76bb09
geekhijabi <bimpeadetunji@rocketmail.com>
2017-11-03 00:19:17 +0100

    merging development

--- client/src/js/components/partials/header.jsx
+++ client/src/js/components/partials/header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 // import { Button } from 'reactstrap';
@@ -7,54 +6,19 @@ import '../../../styles/index.scss';
 /**
  * 
  * 
-=======
-import 'bootstrap/dist/css/bootstrap.css';
-// import Button  from 'reactstrap';
-import '../../../styles/index.scss';
-import SearchBar from '../Partials/SearchBar';
-
-/**
- * 
- * *
->>>>>>> 4d6fd913656271b0926a670b2c6af06bec95200a
  * @class Header
  * @extends {React.Component}
  */
 class Header extends React.Component {
-<<<<<<< HEAD
 /**
 * @return {dom} DomElement
 */
-=======
-  /**
-   * @description COnstructor Function
-   * @param {any} props
-   * @memberof Home
-   * @return {void}
-   */
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: true
-    };
-  }
-  /** 
-   *
-   *@return {dom} DomElement
-   *
-   */
->>>>>>> 4d6fd913656271b0926a670b2c6af06bec95200a
   render() {
     return (
       <div>
         <nav className="navbar navbar-expand-lg navbar-light">
-<<<<<<< HEAD
           <a className="navbar-brand" href="#">
                     <i className="fa fa-eercast" />
-=======
-          <a className="navbar-brand" href="">
-            <i className="fa fa-eercast" />
->>>>>>> 4d6fd913656271b0926a670b2c6af06bec95200a
                         moreRecipe
           </a>
           <button
@@ -77,7 +41,6 @@ class Header extends React.Component {
                 <a className="nav-link btn cornflowerblue" href="./signup.html">Sign up</a>
               </li>
             </ul>
-<<<<<<< HEAD
             <form className="form-inline">
               <input
                             className="form-control mr-sm-2"
@@ -86,9 +49,6 @@ class Header extends React.Component {
                             aria-label="Search"
               />
             </form>
-=======
-            <SearchBar />
->>>>>>> 4d6fd913656271b0926a670b2c6af06bec95200a
           </div>
         </nav>
       </div>
@@ -96,10 +56,6 @@ class Header extends React.Component {
   }
 }
 
-<<<<<<< HEAD
 ReactDOM.render(
   <Header />,);
 
-=======
-export default Header;
->>>>>>> 4d6fd913656271b0926a670b2c6af06bec95200a
