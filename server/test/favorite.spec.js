9a95693b0775b9facb6b91c5c9648593f3fc5bd0
geekhijabi <bimpeadetunji@rocketmail.com>
2018-01-23 18:54:16 +0100

    add more test suite

--- server/test/favorite.spec.js
+++ server/test/favorite.spec.js
@@ -1,30 +1,30 @@
-// import chai from 'chai';
-// import chaiHttp from 'chai-http';
-// import fakeData from './faker';
-// import app from '../app';
-// import db from '../models';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import fakeData from './faker';
+import app from '../app';
+import db from '../models';
 
-// const should = chai.Should();
-// chai.use(chaiHttp);
+const should = chai.Should();
+chai.use(chaiHttp);
 
-// let token;
+let token;
 
-// describe('Favorites', () => {
-//   it('should let unauthorized user access the favorite route', (done) => {
-//     chai.request(app)
-//       .post('/api/v1/recipe/:recipeId/favorite')
-//       .end((err, res) => {
-//         res.body.should.have.property('error').equal('Unauthorised User!');
-//         res.should.have.status(401);
-//         done();
-//       });
-//   });
-//   it('should let unauthorized user access get favorite route', (done) => {
-//     chai.request(app)
-//       .post('/api/v1/favorites')
-//       .end((err, res) => {
-//         res.should.have.status(404);
-//         done();
-//       });
-//   });
-// });
+describe('Favorites', () => {
+  it('should let unauthorized user access the favorite route', (done) => {
+    chai.request(app)
+      .post('/api/v1/recipe/:recipeId/favorite')
+      .end((err, res) => {
+        res.body.should.have.property('error').equal('Unauthorised User!');
+        res.should.have.status(401);
+        done();
+      });
+  });
+  it('should let unauthorized user access get favorite route', (done) => {
+    chai.request(app)
+      .post('/api/v1/favorites')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
+});
