32f907b7a8153fb20d5ea7c7a1d50f412da78d2a
geekhijabi <bimpeadetunji@rocketmail.com>
2018-01-24 08:50:26 +0100

    remove conflict files

--- server/app.js
+++ server/app.js
@@ -14,15 +14,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 routes(app);
-// const env = process.env.NODE_ENV || 'development';
-// if (env === 'production') {
-// for serving static react client app on heroku
+
 app.use('/', express.static('client/dist'));
 app.use('*', express.static('client/dist'));
-// } else {
-// for serving static react client app on server localhost:port
-// app.use('/', express.static(path.resolve(__dirname, '../client/dist')));
-// }
 
 
 app.listen(port);
